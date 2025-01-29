b4e9f33c75a138dd1bc3b4008c2240af543aa5ae
brigii0608 <brigikeeihnath@gmail.com>
2025-01-29 22:12:55 +0100

    Mediaplayer added, buttons play sound on click

---
+++ Project_Floptropica/Project_Floptropica/wwwroot/audioPlayer.js
@@ -0,0 +1,10 @@
+﻿window.playAudio = (fileName) => {
+    let audio = new Audio(`sounds/${fileName}`);
+    audio.play();
+};
+
+window.getSoundFiles = async () => {
+    const response = await fetch("sounds.json");
+    const data = await response.json();
+    return data.files;
+};
