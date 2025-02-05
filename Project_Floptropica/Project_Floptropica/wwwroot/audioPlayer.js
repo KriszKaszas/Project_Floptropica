084f5eb5da2f7be2c120c3ac3b831675f9de8f57
brigii0608 <brigikeeihnath@gmail.com>
2025-02-05 21:12:21 +0100

    fasz

--- Project_Floptropica/Project_Floptropica/wwwroot/audioPlayer.js
+++ Project_Floptropica/Project_Floptropica/wwwroot/audioPlayer.js
@@ -1,16 +1,20 @@
 ﻿window.currentAudio = null;
+window.lastPausedTime = 0; // Store paused time
 
 window.playAudioWithCallback = (fileName, dotNetHelper) => {
     if (window.currentAudio) {
         window.currentAudio.pause();
+        window.currentAudio = null;
     }
 
     window.currentAudio = new Audio(`sounds/${fileName}`);
     window.currentAudio.play();
+    window.lastPausedTime = 0; // Reset pause time
 
     window.currentAudio.onended = () => {
         dotNetHelper.invokeMethodAsync("OnAudioEnded");
         window.currentAudio = null;
+        window.lastPausedTime = 0;
     };
 };
 
@@ -19,10 +23,27 @@ window.toggleAudio = () => {
         if (window.currentAudio.paused) {
             window.currentAudio.play();
         } else {
+            window.lastPausedTime = window.currentAudio.currentTime;
             window.currentAudio.pause();
         }
     }
 };
+
+// Resume audio from where it was paused
+window.resumeAudio = () => {
+    if (window.currentAudio && window.currentAudio.paused) {
+        window.currentAudio.play();
+    }
+};
+
+// Restart audio from the beginning
+window.restartAudio = () => {
+    if (window.currentAudio) {
+        window.currentAudio.currentTime = 0;
+        window.currentAudio.play();
+    }
+};
+
 
 window.getSoundCategories = async () => {
     const response = await fetch("sounds.json");
